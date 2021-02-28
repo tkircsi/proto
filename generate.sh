62e8101beb15054a5fadfd16ad9f7f3c3544608b
Tibor Kircsi <tkircsi@gmail.com>
2021-02-28 21:25:12 +0100

    add person package

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
-protoc --go_out=./ --go_opt=module=github.com/tkircsi/proto/person ./person.proto
+protoc --go_out=./person --go_opt=module=github.com/tkircsi/proto/person ./person.proto
 
 # protoc -I=src/ --go_out=src/ --go_opt=module=github.com/tkircsi/protobuf-test src/simple/simple.proto
